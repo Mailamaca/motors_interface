b5e70c12bfc240e7c9ce89fcd1a32cb73371b8c7
Paolo Tomasin <pooltomasin@hotmail.com>
2020-07-30 10:36:20 +0200

    chore: all communication stuff ok
    build success
    TODO: add PCA9685 class

--- src/motors_interface.cpp
+++ src/motors_interface.cpp
@@ -22,7 +22,7 @@ class MotorsInterface : public rclcpp::Node
 {
   public:
 
-    MotorsInterface() : Node("motors_interface"), count_(0)
+    MotorsInterface() : Node("motors_interface")
     {
       declare_parameters();
       load_parameters();    
@@ -46,8 +46,8 @@ class MotorsInterface : public rclcpp::Node
     std::string cmd_sim_topic_name;     //      = "/sim/cmd";
 
     bool sim_publisher = false;
-    double min_subs_period;
-    Duration timeout_duration;
+    int min_subs_period;
+    int command_period;
     
     rclcpp::Subscription<common_interfaces::msg::VehicleMode>::SharedPtr subs_mode;
     rclcpp::Subscription<common_interfaces::msg::VehicleControl>::SharedPtr subs_autonomous;
@@ -59,8 +59,8 @@ class MotorsInterface : public rclcpp::Node
     rclcpp::TimerBase::SharedPtr timer_subs_embrake_timeout;
     rclcpp::TimerBase::SharedPtr timer_command;
 
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_info;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_sim;
+    rclcpp::Publisher<common_interfaces::msg::VehicleControl>::SharedPtr pub_info;
+    rclcpp::Publisher<common_interfaces::msg::VehicleControl>::SharedPtr pub_sim;
 
     std::atomic<bool> subs_mode_ok;
     std::atomic<bool> subs_autonomous_ok;
@@ -70,25 +70,23 @@ class MotorsInterface : public rclcpp::Node
     std::atomic<float> manual_throttle;
     std::atomic<float> manual_steering_angle;
     std::atomic<float> autonomous_throttle;
-    std::atomic<float> autonomous_steering_angle;
-    
-    size_t count_;
+    std::atomic<float> autonomous_steering_angle;    
 
     void timer_subs_mode_timeout_callback()
     {
-      RCLCPP_INFO(this->get_logger(), "Timeout timer_subs_mode");
+      //RCLCPP_WARN(this->get_logger(), "Timeout timer_subs_mode");
       subs_mode_ok = false;
     }
 
     void timer_subs_autonomous_timeout_callback()
     {
-      RCLCPP_INFO(this->get_logger(), "Timeout timer_subs_autonomous");
+      //RCLCPP_WARN(this->get_logger(), "Timeout timer_subs_autonomous");
       subs_autonomous_ok = false;
     }
 
     void timer_subs_manual_timeout_callback()
     {
-      RCLCPP_INFO(this->get_logger(), "Timeout timer_subs_manual");
+      //RCLCPP_WARN(this->get_logger(), "Timeout timer_subs_manual");
       subs_manual_ok = false;
     }
 
@@ -123,40 +121,49 @@ class MotorsInterface : public rclcpp::Node
 
     void timer_command_callback()
     {
-      RCLCPP_INFO(this->get_logger(), "Timeout timer_command_callback");
+      
+      double throttle = 0.0;
+      double steering_angle = 0.0;    
 
       if (subs_mode_ok) {
         switch (mode) {
 
           case 0: // neutral
-            motors.SetThrottleAndSteer(0.0,0.0);
             break;
 
           case 1: // manual
             if (subs_manual_ok) {
-              motors.SetThrottleAndSteer(manual_throttle,manual_steering_angle);
+              throttle = manual_throttle;
+              steering_angle = manual_steering_angle;
             } else {
-              motors.SetThrottleAndSteer(0.0,0.0);
+              RCLCPP_WARN(this->get_logger(), "Mode=1 but no msgs on manaul topic");
             }
             break;
 
           case 2: // autonomous
             if (subs_autonomous_ok) {
-              motors.SetThrottleAndSteer(manual_throttle,manual_steering_angle);
+              throttle = autonomous_throttle;
+              steering_angle = autonomous_steering_angle;              
             } else {
-              motors.SetThrottleAndSteer(0.0,0.0);
+              RCLCPP_WARN(this->get_logger(), "Mode=2 but no msgs on autonomous topic");
             }
             break;        
-        }           
-
+        }       
       } else {
-        motors.SetThrottleAndSteer(0.0,0.0);
+        RCLCPP_WARN(this->get_logger(), "No msgs on mode topic");
       }
-    }
 
-    void create_publishers() {
-      pub_info = this->create_publisher<std_msgs::msg::String>(cmd_info_topic_name, 10);
-      pub_sim = this->create_publisher<std_msgs::msg::String>(cmd_sim_topic_name, 10);
+      RCLCPP_INFO(this->get_logger(), "Command->\tThrottle:%f\tSteeringAngle:%f", throttle, steering_angle);
+      motors.SetThrottleAndSteer(throttle,steering_angle);
+
+      // msg to simulator
+      if (sim_publisher) {
+        common_interfaces::msg::VehicleControl vehicle_control_msg;
+        vehicle_control_msg.throttle = throttle;
+        vehicle_control_msg.steering_angle = steering_angle;
+        pub_sim->publish(vehicle_control_msg);
+      }
+
 
     }
 
@@ -165,29 +172,35 @@ class MotorsInterface : public rclcpp::Node
       start_timer_subs_mode_timeout();
       start_timer_subs_autonomous_timeout();
       start_timer_subs_manual_timeout();
-      
-      /// TODO: set parameter for timer command
-      timer_command = this->create_wall_timer(
-        50ms, std::bind(&MotorsInterface::timer_command_callback, this)
-        );
-      
+
+      start_timer_command();
     }
 
     void start_timer_subs_mode_timeout() {
+      std::chrono::duration<int, std::milli> timeout_time(min_subs_period);
       timer_subs_mode_timeout = this->create_wall_timer(
-        min_subs_period, std::bind(&MotorsInterface::timer_subs_mode_timeout_callback, this)
+        timeout_time, std::bind(&MotorsInterface::timer_subs_mode_timeout_callback, this)
         );
     }
 
     void start_timer_subs_autonomous_timeout() {
+      std::chrono::duration<int, std::milli> timeout_time(min_subs_period);
       timer_subs_autonomous_timeout = this->create_wall_timer(
-        min_subs_period, std::bind(&MotorsInterface::timer_subs_autonomous_timeout_callback, this)
+        timeout_time, std::bind(&MotorsInterface::timer_subs_autonomous_timeout_callback, this)
         );
     }
 
     void start_timer_subs_manual_timeout() {
+      std::chrono::duration<int, std::milli> timeout_time(min_subs_period);
       timer_subs_manual_timeout = this->create_wall_timer(
-        min_subs_period, std::bind(&MotorsInterface::timer_subs_manual_timeout_callback, this)
+        timeout_time, std::bind(&MotorsInterface::timer_subs_manual_timeout_callback, this)
+        );
+    }
+
+    void start_timer_command() {
+      std::chrono::duration<int, std::milli> timeout_time(command_period);
+      timer_command = this->create_wall_timer(
+        timeout_time, std::bind(&MotorsInterface::timer_command_callback, this)
         );
     }
 
@@ -200,6 +213,12 @@ class MotorsInterface : public rclcpp::Node
 
       subs_manual = this->create_subscription<common_interfaces::msg::VehicleControl>(
         manual_cmd_topic_name, 10, std::bind(&MotorsInterface::subs_manual_callback, this, _1));
+    }
+
+    void create_publishers() {
+      pub_info = this->create_publisher<common_interfaces::msg::VehicleControl>(cmd_info_topic_name, 10);
+      pub_sim = this->create_publisher<common_interfaces::msg::VehicleControl>(cmd_sim_topic_name, 10);
+
     }
 
     void load_parameters() {
@@ -217,6 +236,7 @@ class MotorsInterface : public rclcpp::Node
       this->get_parameter("topics.autonomous_mode", autonomous_mode_topic_name);
       this->get_parameter("topics.cmd_sim", cmd_sim_topic_name);
       this->get_parameter("min_msg_period", min_subs_period);
+      this->get_parameter("command_period", command_period);
       this->get_parameter("sim_publisher", sim_publisher);
       this->get_parameter("max_steering", max_steering);
       this->get_parameter("steer_pwm.center", steer_center);
@@ -262,7 +282,8 @@ class MotorsInterface : public rclcpp::Node
       this->declare_parameter<std::string>("topics.autonomous_mode", "");
       this->declare_parameter<std::string>("topics.cmd_sim", "");
 
-      this->declare_parameter<double>("min_msg_period", 1);
+      this->declare_parameter<int>("min_msg_period", 1000);
+      this->declare_parameter<int>("command_period", 100);
       this->declare_parameter<bool>("sim_publisher", false);
 
       this->declare_parameter<float>("max_steering", 1);
@@ -275,8 +296,7 @@ class MotorsInterface : public rclcpp::Node
       this->declare_parameter<float>("motor_pwm.dead_range", 0);
       this->declare_parameter<float>("motor_pwm.range", 1);
     }
-
-  
+ 
     
     
 
